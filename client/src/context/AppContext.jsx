9ff18247b2897bbfc7f34230f131408ed5ad3793
shashank-wadi <shashankwadi@gmail.com>
2025-08-15 01:55:48 +0530

    uploeding in vercel-client with some changes

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
   const [isLoggedIn, setIsLoggedin] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Add loading state
-  const backendUrl = "http://localhost:4000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 
   axios.defaults.withCredentials = true;
 
